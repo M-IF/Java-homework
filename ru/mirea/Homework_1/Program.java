149aa65fc676c7c873e6e6a70d4088b6fbf63e85
M-IF <115550442+M-IF@users.noreply.github.com>
2022-10-18 15:13:46 +0300

    Программа 6.

--- ru/mirea/Homework_1/Program.java
+++ ru/mirea/Homework_1/Program.java
@@ -1,37 +1,9 @@
 package ru.mirea.Homework_1;
 
-import java.util.Scanner;
-
 public class Program {
-	public static void main (String[] args) {
-		Scanner in = new Scanner(System.in);
-		int size;
-		do {
-			System.out.println("Пожалуйста, введите размер массива.");
-			size = in.nextInt();
-		} while (size <= 0);
-		int arr[] = new int[size];
-		int j = 1;
-		//Цикл do while
-		do {
-			System.out.println("Пожалуйста, введите " + j + "-е число.");
-			arr[j - 1] = in.nextInt();
-			j++;
-		} while (j <= size);
-		int sum, max, min;
-		sum = max = min = arr[0];
-		j = 1;
-		//Цикл while
-		while (j < size) {
-			if (arr[j] > max) {
-				max = arr[j];
-			}
-			if (arr[j] < min) {
-				min = arr[j];
-			}
-			sum += arr[j];
-			j++;
+	public static void main(String[] args) {
+		for (short j = 1; j < 11; j++) {
+			System.out.printf("%d-й элемент = %.2f.\n", j, 1.0 / j);
 		}
-		System.out.printf("Сумма = %d.\nМаксимальный элемент = %d.\nМинимальный элемент = %d.\n", sum, max, min);
 	}
 }
